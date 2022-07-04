a165a59bf1a21cc2331aeabc806b0b80ec6497ca
beyarkay <boydrkane@gmail.com>
2022-07-04 21:23:57 +0200

    Get rust collecting url data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
+use structs::{MunicipalityInfo, Province, RawMunicipalityInfo, SuburbInfo};
+use scraper::Selector;
+
+use crate::structs::GetSuburbDataResult;
+use scraper::Html;
+
+mod structs;
 fn main() {
-    println!("Hello, world!");
+    let province = Province::WesternCape;
+    let municipalities = get_municipalities(&province);
+    let municipality = municipalities.first().unwrap();
+    let suburbs = get_suburbs(&municipality);
+    let suburb = suburbs.first().unwrap();
+    let stage = 1;
+    get_schedule(province, &municipality, &suburb, stage);
+}
+
+fn encode_province(province: &Province) -> u8 {
+    match province {
+        Province::EasternCape => 1,
+        Province::FreeState => 2,
+        Province::Gauteng => 3,
+        Province::KwaZuluNatal => 4,
+        Province::Limpopo => 5,
+        Province::Mpumalanga => 6,
+        Province::NorthWest => 7,
+        Province::NorthernCape => 8,
+        Province::WesternCape => 9,
+    }
+}
+
+fn get_municipalities(province: &Province) -> Vec<MunicipalityInfo> {
+    let encoded_province = encode_province(&province);
+    let url = format!(
+        "https://loadshedding.eskom.co.za/LoadShedding/GetMunicipalities/?Id={encoded_province}"
+    );
+    println!("Getting municipalities via `{}`", url);
+    let val = reqwest::blocking::get(url).unwrap();
+    let mun_info = val
+        .json::<Vec<RawMunicipalityInfo>>()
+        .unwrap()
+        .into_iter()
+        .map(|rmi| rmi.into())
+        .collect::<Vec<MunicipalityInfo>>();
+    // println!("{:?}", mun_info);
+    return mun_info;
+}
+
+fn get_suburbs(mun_info: &MunicipalityInfo) -> Vec<SuburbInfo> {
+    let municipality_id = mun_info.id;
+    // TODO: is there any issue with just giving a really big page size
+    let page_size = 1000;
+    let url = format!(
+        "http://loadshedding.eskom.co.za/LoadShedding/GetSurburbData/?pageSize={page_size}&pageNum=1&id={municipality_id}"
+    );
+    println!(
+        "Getting suburbs for municipality {} via `{url}`",
+        mun_info.name
+    );
+    let val = reqwest::blocking::get(url).unwrap();
+    let suburb_info = val
+        .json::<GetSuburbDataResult>()
+        .unwrap()
+        .Results
+        .into_iter()
+        .map(|rsi| rsi.into())
+        .collect::<Vec<SuburbInfo>>();
+
+    let suburb_infos = suburb_info
+        .into_iter()
+        .filter(|s| s.has_schedule)
+        .collect::<Vec<SuburbInfo>>();
+    // println!("{:#?}", suburb_infos);
+    return suburb_infos;
+}
+
+fn get_schedule(province: Province, mun_info: &MunicipalityInfo, sub_info: &SuburbInfo, stage: u8) {
+    let url = format!(
+        "http://loadshedding.eskom.co.za/LoadShedding/GetScheduleM/{suburb_id}/{stage}/{province_id}/1",
+        suburb_id=sub_info.id,
+        province_id=encode_province(&province)
+    );
+    println!(
+        "Getting schedule for province {:?}, mun_info {}, suburb {}, stage {}, `{url}`",
+        province, mun_info.name, sub_info.name, stage
+    );
+    let val = reqwest::blocking::get(url).unwrap();
+    let html = val.text().unwrap();
+    let document = Html::parse_document(&html);
+    let day_div = Selector::parse("div.scheduleDay").unwrap();
+    println!("{:#?}", document);
 }
---
+++ src/structs.rs
@@ -0,0 +1,110 @@
+use std::fmt::Debug;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub enum Province {
+    EasternCape,
+    FreeState,
+    Gauteng,
+    KwaZuluNatal,
+    Limpopo,
+    Mpumalanga,
+    NorthWest,
+    NorthernCape,
+    WesternCape,
+}
+
+/// Looks something like:
+/// {
+///     "Disabled":false,
+///     "Group":null,
+///     "Selected":false,
+///     "Text":"Beaufort West",
+///     "Value":"336"
+/// }
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct RawMunicipalityInfo {
+    /// The usage of this is unknown
+    Disabled: bool,
+    /// The usage of this is unknown, it's often just `null`
+    Group: Option<String>,
+    /// The usage of this is unknown
+    Selected: bool,
+    /// The name of this municipality
+    Text: String,
+    /// The ID of this municipality
+    Value: String,
+}
+
+/// The information about a municipality.
+#[derive(Debug)]
+pub struct MunicipalityInfo {
+    /// The usage of this is unknown
+    is_disabled: bool,
+    // group: Option<String>,
+    /// The usage of this is unknown
+    is_selected: bool,
+    /// The name of this municipality
+    pub name: String,
+    /// The ID of this municipality
+    pub id: u32,
+}
+
+impl From<RawMunicipalityInfo> for MunicipalityInfo {
+    fn from(raw: RawMunicipalityInfo) -> Self {
+        MunicipalityInfo {
+            is_disabled: raw.Disabled,
+            // group: raw.Group,
+            is_selected: raw.Selected,
+            name: raw.Text,
+            id: raw.Value.parse().expect("Couldn't parse raw.Value"),
+        }
+    }
+}
+
+/// A struct representing the results of a `GetSurburbData`
+/// request. Contains the results and the total number of entries
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct GetSuburbDataResult {
+    pub Results: Vec<RawSuburbInfo>,
+    pub Total: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+pub struct RawSuburbInfo {
+    id: String,
+    text: String,
+    Tot: u32,
+}
+
+/// Information about an eskom suburb
+#[derive(Debug)]
+pub struct SuburbInfo {
+    /// The ID number
+    pub id: u32,
+    /// The name of the suburb
+    pub name: String,
+    /// Not sure what this is
+    total: u32,
+    /// If true, then this suburb will have a schedule associated with it
+    pub has_schedule: bool,
+}
+impl From<RawSuburbInfo> for SuburbInfo {
+    fn from(raw: RawSuburbInfo) -> Self {
+        // let mut chars = raw.id.chars();
+        // chars.next();
+        // chars.next_back();
+        // let id = chars.as_str();
+        SuburbInfo {
+            id: raw.id.parse().expect("Couldn't parse suburb id to u32"),
+            name: raw.text,
+            total: raw.Tot,
+            has_schedule: raw.Tot > 0,
+        }
+    }
+}
