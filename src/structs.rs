0e9b7f8468cb3c73ae3f18fcb4e9df78cb2c8515
beyarkay <boydrkane@gmail.com>
2022-07-16 14:26:48 +0200

    Remove unused code and improve rs docs

--- src/structs.rs
+++ src/structs.rs
@@ -3,109 +3,6 @@ use std::fmt::Debug;
 use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
-#[allow(dead_code)]
-pub enum Province {
-    EasternCape,
-    FreeState,
-    Gauteng,
-    KwaZuluNatal,
-    Limpopo,
-    Mpumalanga,
-    NorthWest,
-    NorthernCape,
-    WesternCape,
-}
-
-/// Looks something like:
-/// {
-///     "Disabled":false,
-///     "Group":null,
-///     "Selected":false,
-///     "Text":"Beaufort West",
-///     "Value":"336"
-/// }
-#[derive(Serialize, Deserialize, Debug)]
-#[allow(non_snake_case)]
-pub struct RawMunicipalityInfo {
-    /// The usage of this is unknown
-    Disabled: bool,
-    /// The usage of this is unknown, it's often just `null`
-    Group: Option<String>,
-    /// The usage of this is unknown
-    Selected: bool,
-    /// The name of this municipality
-    Text: String,
-    /// The ID of this municipality
-    Value: String,
-}
-
-/// The information about a municipality.
-#[derive(Debug)]
-pub struct MunicipalityInfo {
-    /// The usage of this is unknown
-    _is_disabled: bool,
-    // group: Option<String>,
-    /// The usage of this is unknown
-    _is_selected: bool,
-    /// The name of this municipality
-    pub name: String,
-    /// The ID of this municipality
-    pub id: u32,
-}
-
-impl From<RawMunicipalityInfo> for MunicipalityInfo {
-    fn from(raw: RawMunicipalityInfo) -> Self {
-        MunicipalityInfo {
-            _is_disabled: raw.Disabled,
-            // group: raw.Group,
-            _is_selected: raw.Selected,
-            name: raw.Text,
-            id: raw.Value.parse().expect("Couldn't parse raw.Value"),
-        }
-    }
-}
-
-/// A struct representing the results of a `GetSurburbData`
-/// request. Contains the results and the total number of entries
-#[derive(Serialize, Deserialize, Debug)]
-#[allow(non_snake_case)]
-pub struct GetSuburbDataResult {
-    pub Results: Vec<RawSuburbInfo>,
-    pub Total: u32,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[allow(non_snake_case)]
-pub struct RawSuburbInfo {
-    id: String,
-    text: String,
-    Tot: u32,
-}
-
-/// Information about an eskom suburb
-#[derive(Debug)]
-pub struct SuburbInfo {
-    /// The ID number
-    pub id: u32,
-    /// The name of the suburb
-    pub name: String,
-    /// Not sure what this is
-    _total: u32,
-    /// If true, then this suburb will have a schedule associated with it
-    pub has_schedule: bool,
-}
-impl From<RawSuburbInfo> for SuburbInfo {
-    fn from(raw: RawSuburbInfo) -> Self {
-        SuburbInfo {
-            id: raw.id.parse().expect("Couldn't parse suburb id to u32"),
-            name: raw.text,
-            _total: raw.Tot,
-            has_schedule: raw.Tot > 0,
-        }
-    }
-}
-
 /// A multitude of load shedding for a particular suburb
 pub struct ManuallyInputSchedule {
     /// LoadShedding changes, usually in the future (but not always)
@@ -183,7 +80,8 @@ impl From<RawShedding> for Shedding {
     }
 }
 
-/// A loadshedding event that repeats on the same day every month
+/// A loadshedding event that repeats on the same day every month, not yet parsed. See
+/// MonthlyShedding.
 #[derive(Deserialize, Debug)]
 pub struct RawMonthlyShedding {
     /// The time when LoadShedding *should* start.
@@ -195,7 +93,12 @@ pub struct RawMonthlyShedding {
     /// The date of the month which this event occurs on
     date_of_month: u8,
 }
-/// A loadshedding event that repeats on the same day every month
+
+/// A loadshedding event that repeats on the same day every month, parsed into datetimes.
+/// Contains the date of the month, the start time (but the date is always 1 Jan 1970), the end
+/// time (but the date is always 1 Jan 1970 or 2 Jan 1970), a boolean to indicate if the start time and the end
+/// time imply the loadshedding goes over midnight (ie from 22:00 to 00:30) and the stage of the
+/// loadshedding.
 #[derive(Debug)]
 pub struct MonthlyShedding {
     /// The time when LoadShedding *should* start. The date of this member will always be 1 Jan
