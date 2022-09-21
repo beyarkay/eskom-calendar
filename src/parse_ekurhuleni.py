624a2f50a3d65705ae9b9d2546967c6fb8920d25
beyarkay <boydrkane@gmail.com>
2022-09-21 14:30:18 +0200

    Fix: Deduplicate in parse_ekurhuleni.py

--- src/parse_ekurhuleni.py
+++ src/parse_ekurhuleni.py
@@ -10,9 +10,6 @@ warnings.filterwarnings("ignore")
 
 
 def main():
-    print(
-        "WARNING: The downloaded schedule is incorrect for gauteng-ekurhuleni-block-14. See https://github.com/beyarkay/eskom-calendar/issues/55"
-    )
     # Get the path which the pdf should be saved to
     path = f"generated/gauteng-ekurhuleni.pdf"
     url = "https://www.ekurhuleni.gov.za/wp-content/uploads/2021/11/Ekurhuleni-Load-Shedding-Schedule.pdf"
@@ -113,10 +110,11 @@ def main():
         "23:59",
     ]
 
-    pdf["start_time"] = [starts[i // 8] for i in range(pdf.shape[0])]
-    pdf["finsh_time"] = [finshs[i // 8] for i in range(pdf.shape[0])]
     for area in range(1, 32):
         pdf[area] = pd.to_numeric(pdf[area], downcast="integer")
+
+    pdf["start_time"] = [starts[i // 8] for i in range(pdf.shape[0])]
+    pdf["finsh_time"] = [finshs[i // 8] for i in range(pdf.shape[0])]
 
     # Stages are subsets of each other, so add in the extra rows so that stage
     # 2 is a superset of stage 1, stage 3 a superset of stage 2, etc
@@ -131,15 +129,71 @@ def main():
         var_name="date_of_month",
     ).sort_values(["stage", "date_of_month", "start_time"])
 
+    # For some reason block 14 has 2h30 long loadshedding, whereas the rest of
+    # ekurhuleni has 2h long loadshedding. This piece of code fixes that See
+    # https://github.com/beyarkay/eskom-calendar/issues/55"
+    to_alt = {
+        "02:00": "02:30",
+        "04:00": "04:30",
+        "06:00": "06:30",
+        "08:00": "08:30",
+        "10:00": "10:30",
+        "12:00": "12:30",
+        "14:00": "14:30",
+        "16:00": "16:30",
+        "18:00": "18:30",
+        "20:00": "20:30",
+        "22:00": "22:30",
+        "23:59": "00:30",
+    }
+    pdf["finsh_time"] = pdf.apply(
+        lambda row: to_alt[row["finsh_time"]]
+        if row["area"] == 14
+        else row["finsh_time"],
+        axis=1,
+    )
+
     areas = []
     for area_idx in sorted(pdf["area"].unique()):
         area_df = pdf[pdf["area"] == area_idx].sort_values(
             ["date_of_month", "start_time", "stage"]
-        )
+        )[["date_of_month", "start_time", "finsh_time", "stage"]]
+        print(area_df[area_df.stage == 8].head())
+        # Now the big task is to de-duplicate the stages. For example:
+        # date_of_month start_time finsh_time  stage
+        #             1      02:00      04:30      8
+        #             1      04:00      06:30      8
+        # The first two rows should really be one row like:
+        # date_of_month start_time finsh_time  stage
+        #             1      02:00      06:30      8
+        # So this code combines them:
+        area_df = area_df.sort_values(["stage", "date_of_month", "start_time"])
+        df2 = pd.DataFrame(columns=area_df.columns)
+        for i, curr in area_df.iterrows():
+            # the first row always gets added
+            if len(df2) == 0:
+                df2 = pd.concat((df2, curr.to_frame().T), ignore_index=True)
+                continue
+            prev = df2.iloc[-1]
+            # Don't attempt to combine rows of different stages
+            if prev["stage"] != curr["stage"]:
+                df2 = pd.concat((df2, curr.to_frame().T), ignore_index=True)
+                continue
+            # If this row and the previous row overlap in their times => combine them
+            starts_before_finish = pd.to_datetime(prev["start_time"]) <= pd.to_datetime(
+                curr["finsh_time"]
+            )
+            finishes_after_start = pd.to_datetime(prev["finsh_time"]) >= pd.to_datetime(
+                curr["start_time"]
+            )
+            if starts_before_finish and finishes_after_start:
+                prev["finsh_time"] = curr["finsh_time"]
+            else:
+                df2 = pd.concat((df2, curr.to_frame().T), ignore_index=True)
+        area_df = df2.sort_values(["date_of_month", "start_time", "stage"])
+        print(area_df[area_df.stage == 8].head())
         path = f"generated/gauteng-ekurhuleni-block-{int(area_idx)}.csv"
-        area_df[["date_of_month", "start_time", "finsh_time", "stage"]].to_csv(
-            path, index=False
-        )
+        area_df.to_csv(path, index=False)
         print("Saved to " + path)
 
 
