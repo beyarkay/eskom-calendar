c6a6e5bb908d397d85ec6c08597216fad6e235b5
beyarkay <boydrkane@gmail.com>
2022-09-21 13:37:10 +0200

    Fix: Deduplicate in parse_city_power.py

--- src/parse_city_power.py
+++ src/parse_city_power.py
@@ -69,8 +69,10 @@ def main():
     # 00:00-02:30     1              3   9.0
     pdf = pdf.set_index(["duration", "stage"]).stack().reset_index()
     pdf.columns = ["duration", "stage", "date_of_month", "area"]
-
     pdf["stage"] = pd.to_numeric(pdf.stage)
+    pdf["start_time"] = pdf["duration"].apply(lambda x: x.split("-")[0][:5])
+    pdf["finsh_time"] = pdf["duration"].apply(lambda x: x.split("-")[1][:5])
+    pdf = pdf.drop("duration", axis=1)
     # Stages are subsets of each other, so add in the extra rows so that stage
     # 2 is a superset of stage 1, stage 3 a superset of stage 2, etc
     for stage in range(1, 8):
@@ -78,14 +80,47 @@ def main():
         this_stage.stage = stage + 1
         pdf = pd.concat((this_stage, pdf))
 
-    max_area_code = int(pdf["area"].max())
-    pdf["start_time"] = pdf["duration"].apply(lambda x: x.split("-")[0][:5])
-    pdf["finsh_time"] = pdf["duration"].apply(lambda x: x.split("-")[1][:5])
+    # We don't bother including stage == 0
+    pdf = pdf[pdf["stage"] > 0]
+
     areas = []
     for area_idx in sorted(pdf["area"].unique()):
         area_df = pdf[pdf["area"] == area_idx].sort_values(
             ["date_of_month", "start_time", "stage"]
         )
+        # Now the big task is to de-duplicate the stages. For example:
+        # date_of_month start_time finsh_time  stage
+        #             1      02:00      04:30      8
+        #             1      04:00      06:30      8
+        # The first two rows should really be one row like:
+        # date_of_month start_time finsh_time  stage
+        #             1      02:00      06:30      8
+        # So this code combines them:
+        area_df = area_df.sort_values(["stage", "date_of_month", "start_time"])
+        df2 = pd.DataFrame(columns=area_df.columns)
+        for i, curr in area_df.iterrows():
+            # the first row always gets added
+            if len(df2) == 0:
+                df2 = pd.concat((df2, curr.to_frame().T), ignore_index=True)
+                continue
+            prev = df2.iloc[-1]
+            # Don't attempt to combine rows of different stages
+            if prev["stage"] != curr["stage"]:
+                df2 = pd.concat((df2, curr.to_frame().T), ignore_index=True)
+                continue
+            # If this row and the previous row overlap in their times => combine them
+            starts_before_finish = pd.to_datetime(prev["start_time"]) < pd.to_datetime(
+                curr["finsh_time"]
+            )
+            finishes_after_start = pd.to_datetime(prev["finsh_time"]) > pd.to_datetime(
+                curr["start_time"]
+            )
+            if starts_before_finish and finishes_after_start:
+                prev["finsh_time"] = curr["finsh_time"]
+            else:
+                df2 = pd.concat((df2, curr.to_frame().T), ignore_index=True)
+        area_df = df2.sort_values(["date_of_month", "start_time", "stage"])
+
         path = f"generated/city-power-{int(area_idx)}.csv"
         area_df[["date_of_month", "start_time", "finsh_time", "stage"]].to_csv(
             path, index=False
