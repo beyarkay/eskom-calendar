543c11b3a573ca93ec96edaf9b870548d690cb51
Keegan White <keeganthomaswhite@gmail.com>
2023-09-08 10:57:31 +0200

    added logging, improved sorting and added overlap grouping

--- src/aggregate_historical_data.py
+++ src/aggregate_historical_data.py
@@ -1,48 +1,63 @@
+import datetime
 import os
 import yaml
 from dateutil.parser import parse
+import logging
 
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    filename='aggregate_historical_data.log',
+                    filemode='a')
+logger = logging.getLogger()
 NEW_DIRECTORY = "historical_versions"
 AGGREGATED_FILE = "historical_data.yml"
 
 
-def find_overlaps(data):
-    # Sort the data based on start time
-    sorted_data = sorted(data, key=lambda x: parse(x['start']))
-
+def find_overlaps(sorted_data):
     overlaps = []
-
-    # Loop through sorted data
-    for i in range(len(sorted_data) - 1):
-        # If end time of current item is greater than start time of next item
-        if parse(sorted_data[i]['finsh']) > parse(sorted_data[i+1]['start']):
-            overlaps.append((sorted_data[i], sorted_data[i+1]))
-
+    for i in range(len(sorted_data)):
+        for j in range(i + 1, len(sorted_data)):
+            if sorted_data[i]['finsh'] > sorted_data[j]['start']:
+                overlaps.append((sorted_data[i], sorted_data[j]))
+                if sorted_data[j]['start'] > sorted_data[i]['finsh']:
+                    break
     return overlaps
 
 
+def find_erroneous_line(file):
+    with open(file, 'r') as f:
+        for i, line in enumerate(f, start=1):
+            try:
+                yaml.safe_load(line)
+            except Exception as inner_e:
+                print(f"Error on line {i}: {inner_e}")
+                print(f"Line content: {line.strip()}")
+
+
 def load_yaml_files(directory):
+    num_file_import_errors = 0
     files = sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.yml')])
     loadshedding_data = []
     for file in files:
-        with open(file, 'r') as f:
-            loadshedding_data.append(yaml.safe_load(f))
+        try:
+            with open(file, 'r') as f:
+                date_time_str = file.split("_")[-1]
+                date_time_str = date_time_str.split(".")[0]
+                data = yaml.safe_load(f)
+                for change in data["changes"]:
+                    change['commit_time'] = datetime.datetime.fromisoformat(date_time_str)
+                loadshedding_data.append(data)
+        except Exception as e:
+            num_file_import_errors += 1
+            logger.exception(f"Error in file {file}")
+            continue
+    print(f"There were {num_file_import_errors} files that could not be imported. Check aggregate_historical_data.log"
+          f" for details")
     return loadshedding_data
 
 
-def aggregate_data(loadshedding_raw_data):
-    unfiltered_aggregated_data = []
+def resolve_conflicts(sorted_data):
 
-    for d in loadshedding_raw_data:
-        unfiltered_aggregated_data.extend(d.get('changes', []))
-
-    return unfiltered_aggregated_data
-
-
-def resolve_conflicts(aggregated_data):
-    # Here, implement the logic to resolve conflicts
-    # It will involve sorting the entries by 'start' date and identifying & resolving overlaps
-    sorted_data = sorted(aggregated_data, key=lambda x: parse(x['start']))
 
     resolved_data = []
 
@@ -56,22 +71,30 @@ def write_yaml(filtered_aggregated_data, file_path):
         yaml.safe_dump({'historical_data': filtered_aggregated_data}, f)
 
 
+def aggregate_data(data_as_dict, key):
+    aggregated_data = []
+    for entry in data_as_dict:
+        for entries in entry[key]:
+            aggregated_data.append(entries)
+    return aggregated_data
+
+
 if __name__ == "__main__":
-    # raw_data = load_yaml_files(NEW_DIRECTORY)
-    # unfiltered_data = aggregate_data(raw_data)
-    # filtered_data = resolve_conflicts(unfiltered_data)
-    # write_yaml(filtered_data, AGGREGATED_FILE)
-    sample_data = [
-        {'start': '2023-09-04T16:00:00', 'finsh': '2023-09-04T20:00:00', 'stage': 5},
-        {'start': '2023-09-04T19:00:00', 'finsh': '2023-09-04T22:00:00', 'stage': 3},
-        {'start': '2023-09-05T05:00:00', 'finsh': '2023-09-05T10:00:00', 'stage': 6},
-    ]
+    raw_data = load_yaml_files(NEW_DIRECTORY)
+    aggregated_data = aggregate_data(raw_data, 'changes')
+    num_entries_unfiltered = len(aggregated_data)
+    aggregated_data = [entry for entry in aggregated_data if 'start' in entry]
 
-    sorted_data = sorted(sample_data, key=lambda x: parse(x['start']))
+    # Ensuring all 'start' and 'finsh' values are datetime objects
+    for entry in aggregated_data:
+        if isinstance(entry.get('start'), str):
+            entry['start'] = parse(entry['start'])
+        if isinstance(entry.get('finsh'), str):
+            entry['finsh'] = parse(entry['finsh'])
 
-    for entry in sorted_data:
-        print(entry)
-
-    overlaps = find_overlaps(sample_data)
-    for overlap in overlaps:
-        print(f"Overlap between: {overlap[0]} and \n{overlap[1]}")
+    sorted_data = sorted(aggregated_data, key=lambda x: x.get('start'))
+    num_entries_filtered = len(sorted_data)
+    diff = num_entries_unfiltered - num_entries_filtered
+    print(f"{diff} entries were incorrectly formatted and dropped")
+    total_overlaps = 0
+    overlaps = find_overlaps(sorted_data)
